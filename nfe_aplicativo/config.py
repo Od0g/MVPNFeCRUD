924a1ed4c56fa82571699f0a938d4b2786c9b122
Augusto Inacio <153130112+Od0g@users.noreply.github.com>
2025-05-29 01:18:11 +0000

    Deploy

--- nfe_aplicativo/config.py
+++ nfe_aplicativo/config.py
@@ -4,5 +4,12 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///gerenciador_nfe.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma_chave_secreta_muito_segura_para_flash_messages_PADRAO'
+    # Use variável de ambiente para a SECRET_KEY
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma_chave_secreta_muito_segura_para_desenvolvimento_PADRAO'
+
     # Adicionar outras configurações aqui, se necessário
+    # Desative o modo de debug em produção
+    DEBUG = False
+    TESTING = False
+    
+
